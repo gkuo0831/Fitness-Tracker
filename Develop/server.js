fae1e5ef83388615f76b56f77e04385c5247b74e
Grace Kuo <gkuo.github@gmail.com>
2021-02-11 19:06:14 -0700

    created app.use for packages

--- Develop/server.js
+++ Develop/server.js
@@ -4,3 +4,9 @@ const mongoose = require("mongoose");
 
 const app = express();
 const PORT = process.env.PORT || 8080;
+
+app.use(morgan("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
