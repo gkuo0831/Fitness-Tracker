a06ed297213128e1c83481aed56741d3230f3290
Grace Kuo <gkuo.github@gmail.com>
2021-02-11 19:20:24 -0700

    set up app to listen on PORT

--- Develop/server.js
+++ Develop/server.js
@@ -19,3 +19,6 @@ mongoose.connect(MONGODB_URI, {
 
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
+app.listen(PORT, function () {
+  console.log(`App listening on Port ${PORT}`);
+});
