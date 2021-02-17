ad4e224d8294e07127773dabfb8f0b92bb946982
Grace Kuo <gkuo.github@gmail.com>
2021-02-16 18:16:34 -0700

    app.post created

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,5 +1,5 @@
 const { model } = require("mongoose");
-const Workout = require("../models/fitness");
+const Workout = require("../models/workout");
 
 model.exports = function (app) {
   app.get("/api/workouts", function (req, res) {
@@ -7,6 +7,13 @@ model.exports = function (app) {
       .then((data) => {
         res.json(data);
       })
+      .catch((err) => {
+        res.json(err);
+      });
+  });
+  app.post("/api/workouts", function (req, res) {
+    Workout.create({})
+      .then((data) => res.json(data))
       .catch((err) => {
         res.json(err);
       });
