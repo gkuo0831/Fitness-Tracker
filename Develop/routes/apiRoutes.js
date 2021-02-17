ecf366af3731feca6fe213e6746ef38cc948156f
Grace Kuo <gkuo.github@gmail.com>
2021-02-16 18:14:29 -0700

    added app.get

---
+++ Develop/routes/apiRoutes.js
@@ -0,0 +1,14 @@
+const { model } = require("mongoose");
+const Workout = require("../models/fitness");
+
+model.exports = function (app) {
+  app.get("/api/workouts", function (req, res) {
+    Workout.find()
+      .then((data) => {
+        res.json(data);
+      })
+      .catch((err) => {
+        res.json(err);
+      });
+  });
+};
