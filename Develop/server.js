5d86be42c689c3bd1cca7ca05d0b325b68155b9d
Grace Kuo <gkuo.github@gmail.com>
2021-02-18 18:01:01 -0700

    deploying

--- Develop/server.js
+++ Develop/server.js
@@ -11,11 +11,14 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout";
-mongoose.connect(MONGODB_URI, {
-  useNewUrlParser: true,
-  useFindAndModify: false,
-});
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/gym-app34',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
 
 const db = require("./models");
 
