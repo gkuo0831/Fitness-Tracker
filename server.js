fd116fa73712b1c2d506976bf7ab56897701b436
Grace Kuo <gkuo.github@gmail.com>
2021-02-18 22:12:27 -0700

    deploy

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/gym-app34", {
   useFindAndModify: false,
 });
 
-const db = require("../models");
+//const db = require("./models");
 
 // routes
 app.use(require("./routes/apiRoutes"));
